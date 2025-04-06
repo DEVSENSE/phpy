be28389fe46947fd6605038f50f004f16209c0e8
Jakub Míšek <jmisek@gmail.com>
2025-04-06 16:35:21 +0200

    analyze function

--- src/Devsense.PHP.CodeAnalysis/Program.cs
+++ src/Devsense.PHP.CodeAnalysis/Program.cs
@@ -1,11 +1,32 @@
 ﻿using System;
 using Bootsharp;
+using System.Threading.Tasks;
+using PHP.VisualStudio.Language.Nodes.Project.Containers;
+using Devsense.PHP.Nodes.Providers;
+using Devsense.PHP.Nodes.Project;
+using PHP.VisualStudio.Language.Nodes.Builtin;
+using System.Collections.Generic;
+using Devsense.PHP.ControlFlow.Analysis.Errors;
+using Devsense.PHP.Syntax;
+using PHP.VisualStudio.Language.Nodes.Composer.References;
+using System.Threading;
+using System.Linq;
+using PHP.VisualStudio.Language.Nodes.Ast;
+using PHP.VisualStudio.Language.Ast;
+using PHP.VisualStudio.Language.Nodes.Project;
+using System.IO;
+using Devsense.PHP.Text;
 
 public static partial class Program
 {
     public static void Main()
     {
         //OnMainInvoked($"Hello {GetFrontendName()}, .NET here!");
+
+        // setup composer project behavior:
+        ComposerReferencesSetup.CoolDownTime = TimeSpan.Zero;
+        ComposerReferencesSetup.FileChangeCoolDownTime = TimeSpan.Zero;
+        ComposerReferencesSetup.EnableOnlineCache = false;
     }
 
     //[JSEvent] // Used in JS as Program.onMainInvoked.subscribe(..)
@@ -14,6 +35,208 @@ public static partial class Program
     //[JSFunction] // Set in JS as Program.getFrontendName = () => ..
     //public static partial string GetFrontendName();
 
-    //[JSInvokable] // Invoked from JS as Program.GetBackendName()
-    //public static string GetBackendName() => Environment.Version;
+    [JSInvokable] // Invoked from JS as Program.GetBackendName()
+    public static async Task Analyze(string root, IDictionary<string, string> files)
+    {
+        using (var project = new MockProject(root))
+        {
+            foreach (var pair in files)
+            {
+                if (project.ComposerNodesCollection?.TryHandle(pair.Key, false) == true) // first let composer nodes to handle its files
+                {
+                    // userfiles.Add(
+                    //     (fullpath, File.ReadAllText(fullpath))
+                    // );
+                }
+            }
+
+            // wait for composer packages
+            await project.WaitForLoadAsync();
+
+            foreach (var pair in files)
+            {
+                project.Add(pair.Value, pair.Key, out var errors, postponeAnalysis: true/*analyze once all files are parsed*/);
+            }
+
+            // analyze and collect diagnostics
+            project.AnalyseToDo();
+
+            foreach (var file in project.Files)
+            {
+                // // ignore errors in /vendor/ ... expected
+                // if (project.ComposerNodesCollection?.IsVendorFile(project.ProjectDir, file.FileName, out _, out _))
+                // {
+                //     continue;
+                // }
+
+                file.Ast.ContainingSourceUnit.TryGetProperty(typeof(CommonError[]), out var errorsObj);
+                if (errorsObj is CommonError[] errors && errors.Length != 0)
+                {
+                    foreach (var error in errors)
+                    {
+                        Console.WriteLine($"[{error.ErrorInfo.ErrorCode}] {error} at {file.FileName}:{file.Ast.ContainingSourceUnit.GetLineFromPosition(error.Span.Start)}");
+                    }
+                }
+            }
+        }
+    }
+}
+
+class MockProject : ProjectContainer
+{
+    public MockProject(string root)
+    {
+        this.ProjectDir = root;
+    }
+
+    protected override bool ContentServiceCanWatch => false; // not needed // not supported on nodeJS
+
+    internal class ProjectManualReferences : IProjectReferences
+    {
+        public IEnumerable<IReference> References => new IReference[] { ManualNodes.AllReferencedExtensionsNode };
+
+#pragma warning disable CS0067
+        public event EventHandler<RefrencesChangedEventArgs> ReferencesChanged;
+#pragma warning restore CS0067
+    }
+
+    public override string ProjectName => "PROJECT";
+
+    public override string ProjectDir { get; }
+
+    public override ErrorAnalysisSettings.Flags WarningFlags => base.WarningFlags;
+
+    protected override bool EnableComposerNodes { get; } = false;
+
+    public override LanguageFeatures LanguageFeatures => LanguageFeatures.Php84Set | LanguageFeatures.ShortOpenTags;
+
+    protected override IEnumerable<IProjectReferences> ResolveReferences()
+    {
+        yield return new ProjectManualReferences(); // references with PHP manual 
+        yield return new ComposerReferencesProvider().GetReferences(this);
+    }
+
+    public async Task WaitForLoadAsync(CancellationToken cancellation = default)
+    {
+        // output resolved references,
+        // and "wakeup" the lazy references resolution
+        foreach (var r in this.References.OfType<IProjectFolder>())
+        {
+            // 
+        }
+
+        // wait for parsing the packages and caching
+        for (; ; )
+        {
+            if (this.IsLoadPending || ComposerReferencesSetup.PendingWorkers > 0)
+            {
+                await Task.Delay(100);
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
+
+    // list of files to be analyzed
+    // analyze after each file is added, so other tables could be updated and analysis runs in order
+    private HashSet<GlobalCodeNode> _todo_analysis = new HashSet<GlobalCodeNode>();
+
+    public MockProject(string dir = null, bool enableComposerNodes = false)
+    {
+        this.ProjectDir = dir;
+        this.EnableComposerNodes = enableComposerNodes;
+
+        // force initialize framework providers
+        this.UpdateFrameworks();
+    }
+
+    public void AnalyseToDo()
+    {
+        _todo_analysis.Foreach(node => Analysis.AnalyseNode(node));
+        _todo_analysis.Clear();
+    }
+
+    public override void EnqueueNode(IFileInfo file)
+    {
+        _todo_analysis.Add((GlobalCodeNode)file);
+    }
+
+    public override void EnqueueNodes(IEnumerable<IFileInfo> files, bool isreanalysis)
+    {
+        // ignore // invoked by background thread for lazy analysis
+        // we already added every node to _todo_analysis in {EnqueueNode}
+    }
+
+    public override void UpdateAnalysisErrors(IFileInfo node, CommonError[] erritems)
+    {
+        ((IPropertyCollection)node.Ast.ContainingSourceUnit)
+            .SetProperty(erritems);
+    }
+
+    public CommonError[] GetAnalysisErrors(IFileInfo node) =>
+        ((IPropertyCollection)node.Ast.ContainingSourceUnit)
+            .GetProperty<CommonError[]>()
+        ?? Array.Empty<CommonError>();
+
+    /// <summary>
+    /// Parse and add file to the project.
+    /// </summary>
+    public GlobalCodeNode Add(string code, string fname, out IReadOnlyList<CommonError> parse_errors, bool postponeAnalysis = false)
+    {
+        // // projection ?
+        // if (fname.EndsWith(".blade.php", StringComparison.Ordinal))
+        // {
+        //     code = BladeParserTests.TransformBladeTemplate(code);
+        // }
+
+        //
+        var errors = new ErrorSink();
+        var unit = new AstSourceUnit(code, fname, LanguageFeatures);
+        using (var factory = new PhpNodesFactory(unit, errors, LanguageFeatures))
+        {
+            unit.Parse(factory, errors, null);
+
+            parse_errors = errors.Errors();
+
+            // add & analyze
+            return this.Add(unit, code, factory, postponeAnalysis);
+        }
+    }
+
+    /// <summary>
+    /// Add source unit to the container, and set properties as expected by analysis.
+    /// </summary>
+    public GlobalCodeNode Add(AstSourceUnit unit, string code, PhpNodesFactory sink, bool postponeAnalysis = false)
+    {
+        if (unit == null) throw new ArgumentNullException(nameof(unit));
+        if (unit.Ast == null) throw new InvalidOperationException("Ast is null.");
+
+        //if (projection != null)
+        //{
+        //    unit.SetProperty<TextProjection>(projection);
+        //}
+
+        AstHelpers.FinalizeSourceUnitProperties(unit, this, sink, null);
+
+        //// used by INodeSpan and navigation features to obtain the source location (span mapped to primary buffer in case of projection).
+        //// this also maps position to the current snapshot version
+        //unit.SetProperty<Nodes.ISpanMapping>(unit);
+
+        //// add the node span service
+        //// NOTE: add more services as a single object if needed
+        //unit.SetProperty(NodeSpanFactory.Instance); // so far, used by code sense to create span for label and variable nodes correctly
+
+        //
+        var node = GlobalCodeNode.GetNode(unit.Ast);
+        this.UpdateNode(node, code, ContainerUpdateFlags.Added | ContainerUpdateFlags.ChangedInEditor/*force quick analysis*/);
+
+        if (!postponeAnalysis)
+        {
+            this.AnalyseToDo();
+        }
+
+        return node;
+    }
 }
--- src/phpa/src/index.ts
+++ src/phpa/src/index.ts
@@ -2,12 +2,56 @@
 
 import { program } from '@commander-js/extra-typings';
 import bootsharp, { Program } from "../../Devsense.PHP.CodeAnalysis/bin/bootsharp"
-//import { glob } from 'fs';
+import { glob, readFileSync } from 'fs';
 
 // bootsharp.boot().then(() => {
 //     //console.log(Program.getBackendName());
 //     console.log('helo')
 // })
+
+async function collectFiles(cwd: string, globs: string[], log: Logger): Promise<Record<string, string>> {
+    return await new Promise((resolve, reject) => {
+        glob( // it's async
+            globs,
+            { cwd: cwd, },
+            (err, matches) => {
+                if (err) {
+                    log.error(err)
+                    reject(err)
+                }
+
+                let files: Record<string, string> = {}
+                matches.forEach(path => {
+                    files[path] = readFileSync(path).toString('utf8')
+                })
+                resolve(files)
+            }
+        )
+    })
+}
+
+class Logger {
+    constructor(
+        private readonly verbose: boolean
+    ) {
+    }
+
+    notimplemented(what: string) {
+        process.stderr.write(`${what} is not implemented\n`)
+    }
+
+    info(text: string) {
+        if (this.verbose) {
+            process.stdout.write(`${text}\n`)
+        }
+    }
+
+    error(err: Error) {
+        if (err && err.message) {
+            process.stderr.write(`${err.message}\n`)
+        }
+    }
+}
 
 async function main(argv: string[]) {
 
@@ -16,28 +60,38 @@ async function main(argv: string[]) {
         .name('phpa')
         .description('PHP Code Analysis Tool')
         .showHelpAfterError(true)
-        .option('-i, --include <path...>', 'File or directory (including sub-directories) to be indexed.', ['.'])
+        .option('-r, --root <path>', 'Root directory, to which are other parameters relative. Current working directory by default.')
+        .option('-i, --include <path...>', 'Files or directories (including sub-directories) to be indexed.', ['.'])
         .option('-x, --exclude <path...>', 'Files or directories to be excluded from indexing.')
+        .option('--verbose', 'Enable verbose output.')
         .argument('[path...]', 'Files or directories to be analyzed.', ['**/*.php'])
         .action(async (paths, options) => {
 
             //
-            const files = new Set<string>()
+            const log = new Logger(options.verbose == true)
+            const root = options.root ?? process.cwd()
+            const files = await collectFiles(
+                root,
+                (options.include ?? ['.']).flatMap(path => [path, `${path}/**/*.php`]),
+                log
+            )
 
-            options.include.forEach(path => {
-                // glob(path, (err, matches) => {
-                //     matches.forEach(value => files.add(value))
-                // })
-            })
+            if (options.exclude) {
+                log.notimplemented('exclude')
+            }
 
             await bootsharp.boot({})
 
+            log.info(`Indexing ${files.length} file(s) ...`)
+
             //console.log('done')
             
-            Program.analyze()
+            await Program.analyze(root, files)
+
+            log.info(`Done.`)
         })
         .parseAsync(argv)
 }
 
 //
-main(process.argv)
+main(['--verbose', ...process.argv])
